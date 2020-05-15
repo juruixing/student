7b28499650d885523db259cad72bf118437c45cd
etoak <etoak@et.com>
2020-05-15 10:44:35 +0800

    pig同事修改了同一行代码Course.java

--- Course.java
+++ Course.java
@@ -1,5 +1,6 @@
 public class Course{
-	private Integer id;//pig添加了注释
+	private Integer id;//pig添加了注释 pig添加了注释 pig的同事也修改这一行
 	private String course;//pig添加了注释
+	private String course;
 	private Integer score;
 }
